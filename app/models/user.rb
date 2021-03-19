a9a69b0d2e6a459e7e3f18b50d564a7bb0920210
werk stream <werkforlearning@gmail.com>
2021-03-19 15:36:48 +0700

    fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, uniqueness: true, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address"}
-    validates :password_digest, presence: true, length: {minimum: 6}
+    validates :password, presence: true, length: {minimum: 6}
 end
