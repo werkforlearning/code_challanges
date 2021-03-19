ee818420e06d37c913a26dd54f6699173e62524f
werk stream <werkforlearning@gmail.com>
2021-03-19 16:05:00 +0700

    added login and logout feature

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "You're logged in!"
+        else
+            flash[:alert] = "Invalid credentials"
+            render :new
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to sign_up_path, notice: "Logged out"
+    end
+end
