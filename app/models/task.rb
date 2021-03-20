aca44afeffe228312eea74e7c1edca8019cfaa82
werkforlearning <72249520+werkforlearning@users.noreply.github.com>
2021-03-20 07:17:15 +0700

    add default due_date to 3 days from now

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,4 +11,8 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :due_date, presence: true
 
+  after_initialize do
+    self.due_date ||= 3.days.from_now
+  end
+
 end
